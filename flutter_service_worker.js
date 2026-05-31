df558a21d383eb5f7ed5a4c8dd566d2da9e7ab40
nitish347 <nitishchaurasiya3470@gmail.com>
2026-05-31 12:56:31 +0530

    Built web
    Branch: main
    Commit: c7d884807531eeb13f1eccadb8bd4fc25cde9f95 (dirty)
    
    package:peanut 6.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a00d1b88b06318a17aff91525bd1c7f2
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0062a29d2742e2ec07f66b0170a1eaaa",
+"flutter_bootstrap.js": "43d12f4075caabc9a3ac5249a5c2e521",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
